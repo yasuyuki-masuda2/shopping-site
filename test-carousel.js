7f6e169967b5fc481d949a7db04e078c6d00f6b4
EC2 Default User <ec2-user@ip-172-31-87-181.ec2.internal>
2018-11-01 12:24:17 +0000

    sp set

--- test-carousel.js
+++ test-carousel.js
@@ -9,3 +9,18 @@ $(document).ready(function(){
         }
     );
 });
+
+
+$(window).load(function(){
+  $(".pro_img_small img").click(function(){
+    var img_src = $(this).attr("src");
+    $(".pro_img_left img").attr("src", img_src);
+  });
+});
+
+
+$(document).ready(function(){
+  $(".header_menu").click(function(){
+    $(".header_hunba").slideToggle();
+  });
+});
